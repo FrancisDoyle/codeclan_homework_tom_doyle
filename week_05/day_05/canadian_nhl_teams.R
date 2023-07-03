8c37b08fb3e2a6fe1da17718362a978a438c505e
FrancisDoyle <134193494+FrancisDoyle@users.noreply.github.com>
2023-07-03 09:26:46 +0100

    p

--- week_05/day_05/canadian_nhl_teams.R
+++ week_05/day_05/canadian_nhl_teams.R
@@ -1,11 +1,10 @@
 library(shiny)
 library(leaflet)
-library(tidyverse)
-library(readxl)
+library(readr)
+library(ggplot2)
 
 overview <- read_csv("data/overview.csv")
-
-calgary_statistics <- read_xlsx("data/calgary2.xlsx")
+standings <- read_csv("data/standings.csv")
 
 ui <- fluidPage(
   titlePanel("Canadian NHL Teams"),
@@ -16,195 +15,16 @@ ui <- fluidPage(
                leafletOutput("map", width = "1000", height = "600")
              )
     ),
-    
     tabPanel("Calgary",
-             id = "#tab-calgary",
-             fluidRow(
-               column(width = 1,
-                       imageOutput("calgary_icon")
-               )
-             ),
              fluidRow(
                column(width = 3,
-                      radioButtons("Pos", "Position",
-                                   choices = c("Forward", "Defencemean", "Goaltenders"),
-                                   selected = "Forward")
-               ),
-               column(width = 9,
-                      "Table content goes here"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Edmonton",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
+                      imageOutput("calgaryImage")
                ),
                column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
+                      plotOutput("calgary_graph", width = "600", height = "400")
                ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Montreal",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
-               ),
-               column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Ottawa",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
-               ),
-               column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Toronto",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
-               ),
-               column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Winnipeg",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
-               ),
-               column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               )
-             )
-    ),
-    
-    tabPanel("Vancouver",
-             fluidRow(
-               column(width = 12,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 6,
-                      "test"
-               ),
-               column(width = 6,
-                      "test"
-               )
-             ),
-             fluidRow(
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
-               ),
-               column(width = 4,
-                      "test"
+               column(width = 3,
+                      plotOutput("calgary_bar", width = "100%", height = "500px")
                )
              )
     )
@@ -236,31 +56,40 @@ ui <- fluidPage(
                      "<b>Arena:</b> ", overview$Arena, "<br>",
                      "<b>Capacity:</b> ", overview$Capacity, "<br>",
                      "<b>Founded:</b> ", overview$Founded, "<br>",
-                     "<b>Standings:</b> ", overview$Standings, "<br>", 
-                     "<a href='#tab-calgary'>Open Team Tab</a>"
+                     "<b>Standings:</b> ", overview$Standings, "<br>"
                    )
         )
     })
-    #output$calgary_icon <- renderImage({
-     # matrix(
-        #image("data/icons/calgary.png", alt = "Calgary Icon", width = "100px", height = "100px"),
-        #nrow = 1, ncol = 1, byrow = TRUE
-      #)
-    #}, deleteFile = FALSE)
     
-    # FUCKING FINALLY
+    output$calgary_graph <- renderPlot({
+      calgary_standings <- standings[standings$team == "calgary", ]
+      ggplot(calgary_standings, aes(x = year, y = place)) +
+        geom_line(color = "red") +
+        geom_point(color = "yellow") +
+        labs(x = "Year", y = "Place") +
+        xlim(min(calgary_standings$year), max(calgary_standings$year)+1) +
+        scale_y_continuous(breaks = 1:32, trans = "reverse") +
+        theme(panel.grid = element_blank(),
+              panel.background = element_blank())
+    })
     
-    output$calgary_icon <- renderImage({
+    
+    
+    
+    output$calgaryImage <- renderImage({
       list(src = "data/icons/calgary.png",
-           alt = "Calgary Icon",
-           width = "100px",
-           height = "100px")
+           width = "100%",
+           height = "auto")
     }, deleteFile = FALSE)
     
+    
+    output$calgary_bar <- renderPlot({
+      calgary_data <- overview[overview$Name == "Calgary", ]
+      ggplot(data, aes(fill=Avg, y= Capacity, x= Name)) + 
+        geom_bar(position="stack", stat="identity")
+    })
   }
-  
   
   
   shinyApp(ui, server)
   
-  # checkout plotly for the stats
